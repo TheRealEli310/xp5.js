6fc57f8b0d3441aedb097bade7113fe5b3f16455
TheRealEli310 <33650824+TheRealEli310@users.noreply.github.com>
2017-12-09 23:13:47 -0800

    Update keycheck.js

--- cookies/product keys/keycheck.js
+++ cookies/product keys/keycheck.js
@@ -1,1 +1,36 @@
-
+var key;
+var paid;
+document.cookie = "key=Key:KeyNull";
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function checkCookie() {
+    var username = getCookie("key");
+    if (key != "") {
+        alert("Key: " + key);
+    } else {
+        username = prompt("Please enter your product key:", "");
+        if (key != "" && key != null) {
+            setCookie("key", key, 365);
+        }
+    }
+}
+checkCookie();
